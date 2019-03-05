f9c77ebb4edb7361efa422ca29b1a39c193e0eac
runtimers <ricmartinez2@protonmail.com>
2019-03-04 22:19:14 -0800

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-        $(this).toggleClass('open');
-        $('.menu-overlay').toggleClass('open');
-     });
+//$(document).ready(function(){
+//   $('.hamburger').click(function(){
+//        $(this).toggleClass('open');
+//        $('.menu-overlay').toggleClass('open');
+//     });
+//});
+
+$('.hamburger').on('click', function () {
+  $('.menu-overlay').addClass('open');
+});
+
+$( '.menu-overlay' ).on("click", function(){
+  $('.menu-overlay').removeClass('open');
 });
